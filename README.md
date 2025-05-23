65a577ee74c3fcf616280091320eeb662d48d342
rafagalind <rafaelangelgalindohidalgo@gmail.com>
2025-05-23 08:57:31 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Aciertos de Apuestas
+App para análisis inteligente de apuestas deportivas.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Aciertos de Apuestas</h1>
+      <p>Bienvenido a tu app de análisis de apuestas deportivas.</p>
+    </div>
+  );
+}
+
+export default App;
